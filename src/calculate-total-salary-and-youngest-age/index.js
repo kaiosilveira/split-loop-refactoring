3c54097f7b60dd9e3ba50b3117ae088270675803
Kaio Silveira <silveira.kaio@icloud.com>
2024-03-25 08:48:17 +0000

    extract `youngestAge(people)` function

--- src/calculate-total-salary-and-youngest-age/index.js
+++ src/calculate-total-salary-and-youngest-age/index.js
@@ -1,16 +1,19 @@
 export function calculateTotalSalaryAndYoungestAge({ people }) {
-  let youngest = people[0] ? people[0].age : Infinity;
-  for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
-  }
-
-  return { youngestAge: youngest, totalSalary: totalSalary(people) };
+  return { youngestAge: youngestAge(people), totalSalary: totalSalary(people) };
 }
 
 function totalSalary(people) {
   let result = 0;
   for (const p of people) {
     result += p.salary;
+  }
+  return result;
+}
+
+function youngestAge(people) {
+  let result = people[0] ? people[0].age : Infinity;
+  for (const p of people) {
+    if (p.age < result) result = p.age;
   }
   return result;
 }
